372e9a18f2c9ffc2a5a680e9fa986f3b21979ef8
人间楷模张灵玉 <31953979+MeALiYeYe@users.noreply.github.com>
2025-12-05 17:14:23 +0800

    Update Mihogo.sh

--- Mihomo/Mihogo/Mihogo.sh
+++ Mihomo/Mihogo/Mihogo.sh
@@ -3,7 +3,7 @@ set -e
 
 # 确保 bin 目录和 Manage.sh 存在
 mkdir -p "$HOME/bin"
-curl -sL https://raw.githubusercontent.com/MeALiYeYe/ProxyConfigFiles/refs/heads/main/Mihomo/Mihogoo/Manage.sh -o "$HOME/bin/Manage.sh"
+curl -sL https://raw.githubusercontent.com/MeALiYeYe/ProxyConfigFiles/refs/heads/main/Mihomo/Mihogo/Manage.sh -o "$HOME/bin/Manage.sh"
 chmod +x "$HOME/bin/Manage.sh"
 
 # 确保 PATH 中有 $HOME/bin
