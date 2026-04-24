a4af42e859ddd5ea1e45e9648ae4dab608a95344
人间楷模张灵玉 <31953979+MeALiYeYe@users.noreply.github.com>
2026-04-25 00:49:27 +0800

    Update Mihogo.sh to ensure bin directory is in PATH
    Added a check to ensure the bin directory is in the PATH.

--- Mihomo/Mihogo/Mihogo.sh
+++ Mihomo/Mihogo/Mihogo.sh
@@ -1,13 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -e
 
+# 获取脚本
 mkdir -p "$HOME/bin"
 curl -sL https://raw.githubusercontent.com/MeALiYeYe/ProxyConfigFiles/refs/heads/main/Mihomo/Mihogo/Manage.sh -o "$HOME/bin/Manage.sh"
 chmod +x "$HOME/bin/Manage.sh"
 
+# 确保Bin目录存在
 if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
     echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
     export PATH="$HOME/bin:$PATH"
 fi
 
+# 开始部署
 Manage.sh deploy
